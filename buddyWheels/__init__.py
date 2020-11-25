b2eebbf4cfe815d5b9ec1abcb806aa756d244a12
juniorjvn <junior-jvn@live.com>
2020-11-25 02:18:21 -0500

    add requirements to readme

--- buddyWheels/__init__.py
+++ buddyWheels/__init__.py
@@ -6,7 +6,7 @@ import os
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '7cqAembdv221jlq1oplyJfYOsbznTCPk'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
